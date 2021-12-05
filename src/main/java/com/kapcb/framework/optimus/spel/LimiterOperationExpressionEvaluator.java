57f7c7b808e788d4a9d49761d6ebaa13515723ad
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-05 16:20:37 +0800

    limiter operation expression evaludation

--- src/main/java/com/kapcb/framework/optimus/spel/LimiterOperationExpressionEvaluator.java
+++ src/main/java/com/kapcb/framework/optimus/spel/LimiterOperationExpressionEvaluator.java
@@ -1,12 +1,18 @@
 package com.kapcb.framework.optimus.spel;
 
 import com.kapcb.framework.optimus.limit.Limiter;
-import com.kapcb.framework.optimus.model.ExpressionRootObject;
+import com.kapcb.framework.optimus.model.LimiterExpressionRootObject;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.expression.AnnotatedElementKey;
+import org.springframework.context.expression.BeanFactoryResolver;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.util.Assert;
 
-import java.beans.Expression;
 import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -35,7 +41,33 @@ public class LimiterOperationExpressionEvaluator extends CustomerExpressionEvalu
                                                                      Class<?> targetClass,
                                                                      Map<String, Object> injectArgs,
                                                                      BeanFactory beanFactory) {
-        new ExpressionRootObject()
+        LimiterExpressionRootObject limiterExpressionRootObject = new LimiterExpressionRootObject(limiter, method, args, target, targetClass);
+        CustomerEvaluationContext customerEvaluationContext = new CustomerEvaluationContext(limiterExpressionRootObject, targetMethod, args, getParameterNameDiscoverer());
+        if (MapUtils.isNotEmpty(injectArgs)) {
+            for (String argKey : injectArgs.keySet()) {
+                customerEvaluationContext.setVariable(argKey, injectArgs.get(argKey));
+            }
+        }
+
+        if (Objects.nonNull(beanFactory)) {
+            customerEvaluationContext.setBeanResolver(new BeanFactoryResolver(beanFactory));
+        }
+        return customerEvaluationContext;
+    }
+
+    public Object key(String keyExpression, AnnotatedElementKey annotatedElementKey, EvaluationContext evaluationContext) {
+        Assert.hasLength(keyExpression, "key expression can not be null");
+        return getExpression(keyExpression, annotatedElementKey, this.keyCache).getValue(evaluationContext);
+    }
+
+    public Boolean condition(String conditionExpression, AnnotatedElementKey annotatedElementKey, EvaluationContext evaluationContext) {
+        Assert.hasLength(conditionExpression, "condition expression can not be null");
+        return getExpression(conditionExpression, annotatedElementKey, this.conditionCache).getValue(evaluationContext, Boolean.class);
+    }
+
+    public Boolean unless(String unlessExpression, AnnotatedElementKey annotatedElementKey, EvaluationContext evaluationContext) {
+        Assert.hasLength(unlessExpression, "unless expression can not be null");
+        return getExpression(unlessExpression, annotatedElementKey, this.unlessCache).getValue(evaluationContext, Boolean.class);
     }
 
     void clear() {
@@ -43,4 +75,5 @@ public class LimiterOperationExpressionEvaluator extends CustomerExpressionEvalu
         this.conditionCache.clear();
         this.unlessCache.clear();
     }
+
 }
